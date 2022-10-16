7953e3d369482035989bc9d4e6a510f9727e1c07
saad <saad.moumou.coder@gmail.com>
2022-10-16 22:14:31 +0100

    create function show & clear

--- main.js
+++ main.js
@@ -1,59 +1,111 @@
+//get value
+let InputTitel = document.querySelector("#InputTitel");
+//Checked radio
+let checkF_B;
+let priority_menu = document.querySelector("#priority_menu");
+let status_menu = document.querySelector("#status_menu");
+let input_Date = document.querySelector("#input_Date");
+let input_description = document.querySelector("#InputComment");
+
 
 //Button Save Form
 let Btn_Save = document.querySelector("#Btn_Save");
+//Button clear Form
+let Btn_clear = document.querySelector("#Btn_clear");
+
+
 //Function Click button save
 Btn_Save.addEventListener("click",(e)=>{
     e.preventDefault();
+    if(Radio_Feature.checked == true){
+        checkF_B = document.querySelector('#Radio_Feature').value;
+    }else if(Radio_Bug.checked == true){
+        checkF_B = document.querySelector('#Radio_Bug').value;
+    }
     Add();
+    Clear();
 })
 
-//Button clear Form
-let Btn_clear = document.querySelector("#Btn_clear");
 //Function Click button clear 
 Btn_clear.addEventListener("click",(e)=>{
     e.preventDefault();
 })
 
-//Function Add Task
-// document.querySelector("#Task_Form").addEventListener("submit",(e) =>{
-function Add(e){
-
-    let InputTitel = document.querySelector("#InputTitel").value;
-    let Radio_Feature = document.querySelector("#Radio_Feature").value;
-    let Radio_Bug = document.querySelector("#Radio_Bug").value;
-    let priority_menu = document.querySelector("#priority_menu").value;
-    let status_menu = document.querySelector("#status_menu").value;
-    let input_Date = document.querySelector("#input_Date").value;
-    let InputComment = document.querySelector("#InputComment").value;
+//Create Array dataTask and check if you empty!
+let dataTask;
+if(localStorage.tasks != null){
+    dataTask = JSON.parse(localStorage.tasks);
+}else{
+    dataTask = [];
+}
 
 
-    if(InputTitel == "" || priority_menu == "" || status_menu == "" || input_Date == "" || InputComment == ""){
+//Function Clear
+function Clear(){
+    InputTitel.value = "";
+    priority_menu.value = "";
+    status_menu.value = "";
+    input_Date.value = "";
+    InputComment.value = "";
+}
+
+//Create
+//Function Add_Task 
+function Add(){
+    let newTask = {
+        titel : InputTitel.value,
+        checkFB : checkF_B,
+        priority : priority_menu.value,
+        status : status_menu.value,
+        date : input_Date.value,
+        description : input_description.value,
+    };
+    console.log(newTask);
+
+    //Add Elements To Array
+    dataTask.push(newTask);
+    //Create Local Storage
+    localStorage.setItem('tasks',JSON.stringify(dataTask));
+
+    if(InputTitel.value == "" || priority_menu.value == "" || status_menu.value == "" || input_Date.value == "" || InputComment.value == ""){
             alert("Please Fill in Fields");
-    }else{
-        const list = document.querySelector("#to-do-tasks");
-        const row = document.createElement("div");
-
-        row.innerHTML = `
-        <button class="bg-light d-flex align-items-center border-0 w-100 mb-2 py-3 px-0">
-        <div class="fs-4 text-success">
-			<i class="fa-solid fa-circle-question px-3"></i>
-		</div>
-		<div class="text-start">
-			<div class="fw-bolder fs-5">${InputTitel}</div>
-					<div class="pb-1">
-							<div class="opacity-50">#1 created in ${input_Date}</div>
-							<div class="fw-bold" title="${InputComment}">${InputComment}</div>
-					</div>
-		<div class="">
-			<span class="badge text-bg-primary">${priority_menu}</span>
-			<span class="badge text-bg-secondary">${Radio_Feature}</span>
-		</div>
-		</div>
-        </button>
-        `;
-
-        list.appendChild(row);
     }
-
-    
 };
+
+//Read
+//Function Show_Task
+function Show_Task(){
+    const list = document.querySelector("#to-do-tasks");
+    const row = document.createElement("div");
+    for(let i=0; i<dataTask.length; i++){
+        if(dataTask[i].status == "To_do"){
+            row.innerHTML +=`
+                <button class="bg-light d-flex align-items-center border-0 w-100 mb-2 py-3 px-0">
+                <div class="fs-4 text-success">
+                    <i class="fa-solid fa-circle-question px-3"></i>
+                </div>
+                <div class="text-start">
+                    <div class="fw-bolder fs-5">${dataTask[i].titel}</div>
+                            <div class="pb-1">
+                                    <div class="opacity-50">#${i+1} created in ${dataTask[i].date}</div>
+                                    <div class="fw-bold" title="${dataTask[i].description}">${dataTask[i].description}</div>
+                            </div>
+                <div class="">
+                    <span class="badge text-bg-primary">${dataTask[i].priority}</span>
+                    <span class="badge text-bg-secondary">${dataTask[i].checkFB}</span>
+                </div>
+                </div>
+                </button>
+                `;
+
+            list.appendChild(row);
+            }else if(dataTask[i].status == "In Progress"){
+
+            }else if(dataTask[i].status == "Done"){
+
+            }
+    
+    } 
+}
+//Show Tasks
+Show_Task();
